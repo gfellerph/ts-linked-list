403188040921d0b174b1d581953e4b63c7623762
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2019-01-11 21:59:29 +0700

    Emit umd modules and a minified version

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,42 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
+import {uglify} from 'rollup-plugin-uglify';
 
-export default {
+export default [{
   input: 'src/LinkedList.ts',
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
+      format: 'umd',
+      name: 'LinkedList',
     },
     {
       file: pkg.module,
       format: 'es',
-    },
+    }
   ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
-plugins: [
+  plugins: [
     typescript({
       typescript: require('typescript'),
     }),
   ],
+},
+{
+  input: 'src/LinkedList.ts',
+  output: {
+    file: pkg.min,
+    format: 'umd',
+    name: 'LinkedList',
+  },
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+    uglify()
+  ]
 }
+]
