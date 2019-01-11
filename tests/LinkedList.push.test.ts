e2406ca49715bf5b2440ccf08d681cfd99209ff6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-11 06:44:32 +0000

    Deploy tuelsch/linked-list to github.com/tuelsch/linked-list.git:gh-pages

--- tests/LinkedList.push.test.ts
+++
@@ -1,9 +0,0 @@
-import LinkedList from '../src/LinkedList';
-
-describe('LinkedList.push', () => {
-  it('Pushes a new node to the end of the list', () => {
-    const list = new LinkedList(1, 2);
-    list.push(3);
-    expect(list.toArray()).toEqual([1, 2, 3]);
-  });
-});
