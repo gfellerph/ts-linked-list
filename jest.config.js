fc05eb53e4ba82cebc88a42bffddbb0224e50da7
Philipp Gfeller <tuelsch@gmail.com>
2019-04-02 18:20:47 +0800

    Specify coverage source

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts'],
   globals: {
     'ts-jest': {
       tsConfig: './tsconfig.test.json',
